13b4650c356e8a1b39266f7ff47274fa4578d263
tomerlevyg <47150256+tomerlevyg@users.noreply.github.com>
2022-07-10 21:03:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello From Docker');
+  res.send('Hello From Docker Updated v2');
 });
 
 app.listen(PORT, HOST);
